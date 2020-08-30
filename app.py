57e79075f7f5b52eb78aeefbfc643ee6f1509e3a
miriam99 <nwaogarakunnkechi@gmail.com>
2020-08-30 22:51:55 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+from pycaret.classification import load_model, predict_model
+import pandas as pd
+import pickle
+import numpy as np
+import jinja2
+
+app = Flask(__name__)
+
+model = load_model('Final_model3')
+cols = ['Age', 'No_of_Dependents','Available_balance','Ledger_balance', 'Income','Credit_score','Loan_Amount','Gender','Occupation',
+       'Marital_Status', 'Employment_Status','Education_Status']
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict',methods=['POST'])
+
+def predict():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction = predict_model(model, data=data_unseen)
+    prediction = int(prediction.Label[0])
+    return render_template('home.html',prediction_text='The output is {}'.format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True,port = 5009)
