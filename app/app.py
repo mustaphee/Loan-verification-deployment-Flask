c57064427534f35557c0d66f739b27296f676634
Yusuff Mustapha <yusuf.m@touchcore.com.ng>
2020-10-07 16:45:15 +0100

    move modules

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-import model
+from . import model
 
 
 app = FastAPI(
